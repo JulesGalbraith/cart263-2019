caed24e5a541692b026d93a6cac2d3df2b1525a2
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2019-01-13 22:27:48 -0500

    Assignement 1: added new function setting interval at which food changes direction

--- Assignment1/js/script.js
+++ Assignment1/js/script.js
@@ -9,7 +9,9 @@ let avatar = {
   alive: true,
   r: 255,
   g: 226,
-  b: 100
+  b: 100,
+  grow: 3,
+  shrink: 0.25
 }
 
 let food = {
@@ -18,12 +20,15 @@ let food = {
   size: 50,
   r: 100,
   g: 204,
-  b: 255
-}
+  b: 255,
+  maxV: 3,
+  vx: 1,
+  vy: 1,
+  }
 
 let appetite;
 let deFont;
-
+let direction;
 
 function preload() {
   backgroundImg = loadImage("assets/images/starfield.jpg");
@@ -31,29 +36,29 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1000,600);
 
+  createCanvas(1000, 600);
   textAlign(CENTER);
+  randomizeFood();
 
-  food.x = random(0,width);
-  food.y = random(0,height);
+  setInterval(positionFood,1500);
 
 }
 
 function draw() {
- background(backgroundImg);
+  background(backgroundImg);
 
- if (avatar.alive) {
- updateAvatar();
- displayAvatar();
+  if (avatar.alive) {
+    updateAvatar();
+    displayAvatar();
 
- displayFood();
- collision();
-}
-
-else {
-  endGame();
-}
+    updateFood();
+    displayFood();
+    collision();
+  }
+   else {
+    endGame();
+  }
 
 }
 
@@ -61,8 +66,8 @@ function updateAvatar() {
   avatar.x = mouseX;
   avatar.y = mouseY;
 
-  avatar.size -= 0.25;
-  avatar.size = constrain(avatar.size,0,avatar.maxSize);
+  avatar.size -= avatar.shrink;
+  avatar.size = constrain(avatar.size, 0, avatar.maxSize);
 
   if (avatar.size === 0) {
     avatar.alive = false;
@@ -73,8 +78,8 @@ function displayAvatar() {
 
   push();
   noStroke();
-  fill(avatar.r,avatar.g,avatar.b);
-  ellipse(avatar.x,avatar.y,avatar.size);
+  fill(avatar.r, avatar.g, avatar.b);
+  ellipse(avatar.x, avatar.y, avatar.size);
   pop();
 }
 
@@ -82,26 +87,19 @@ function displayFood() {
 
   push();
   noStroke();
-  fill(food.r,food.g,food.b);
-  ellipse(food.x,food.y,food.size);
+  fill(food.r, food.g, food.b);
+  ellipse(food.x, food.y, food.size);
   pop();
 }
 
 function collision() {
-  appetite = dist(food.x,food.y,avatar.x,avatar.y);
+  appetite = dist(food.x, food.y, avatar.x, avatar.y);
 
-  if (appetite < (food.size/2 + avatar.size/2) ){
-    avatar.size += 3
-    avatar.size = constrain (avatar.size,0,avatar.maxSize);
+  if (appetite < (food.size / 2 + avatar.size / 2)) {
+    avatar.size += avatar.grow;
+    avatar.size = constrain(avatar.size, 0, avatar.maxSize);
 
-    food.x = random(0,width);
-    food.y = random(0,height);
-
-    food.g = random(0,255);
-    food.r = random(0,255);
-
-    avatar.b = random(0.255);
-    avatar.g = random(0,255);
+    randomizeFood();
   }
 }
 
@@ -111,11 +109,51 @@ function endGame() {
   push();
   textFont(deFont);
   textSize(100);
-  fill(255,0,0);
-  text("TOUGH LUCK",width/2,height/2);
+  fill(255, 0, 0);
+  text("TOUGH LUCK", width / 2, height / 2);
   pop();
 
   keyPressed();
+
+}
+
+function randomizeFood() {
+
+  food.x = random(0, width);
+  food.y = random(0, height);
+
+  food.g = random(0, 255);
+  food.r = random(0, 255);
+
+  avatar.b = random(0.255);
+  avatar.g = random(0, 255);
+
+}
+
+function updateFood() {
+
+  food.x += food.vx;
+  food.y += food.vy;
+
+  if (food.x > width) {
+    food.x -= width;
+  }
+  if (food.x < 0) {
+    food.x += width;
+  }
+  if (food.y > height) {
+    food.y -= height;
+
+  }
+  if (food.y < 0) {
+    food.y += height;
+  }
+}
+
+function positionFood() {
+
+  food.vx = food.maxV*random(-food.maxV,food.maxV)
+  food.vy = food.maxV*random(-food.maxV,food.maxV)
 
 }
 
