68743ad7c5fefaf71d510c7f417b18b70161f249
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2019-01-27 12:56:56 -0500

    Added assignment 3

---
+++ Assignment3/js/script.js
@@ -0,0 +1,65 @@
+"use strict"
+
+$(document).ready(setup);
+
+let secretsFound = 1;
+let secrets = 6;
+let bckImg;
+let selfDestruct = 0;
+
+
+function setup() {
+  setInterval(update, 500);
+  $("span").on("click",spanClicked);
+  $(".secret").on("mouseover",mouseWave);
+  $("#gotcha").hide();
+}
+
+function draw() {
+  destruct();
+}
+
+function update() {
+  $("span").each(updateSpan);
+}
+
+function updateSpan() {
+
+  let probability;
+  probability = Math.random();
+
+  if (probability < 0.1) {
+    $(this).removeClass("redacted");
+    $(this).addClass("revealed");
+  }
+}
+
+function spanClicked(){
+
+  $(this).addClass("redacted");
+  $(this).removeClass("revealed");
+}
+
+function mouseWave(){
+  $(this).addClass("revealedSecret");
+  $(this).off("mouseover")
+  $(this).removeClass("secret");
+
+if (secretsFound < secrets){
+ secretsFound += 1
+ $(".count").text(secretsFound);
+}
+
+if (secretsFound === secrets) {
+  $(".count").text("all the");
+  selfDestruct === 1;
+}
+}
+
+function destruct() {
+  if (selfDestruct === 1); {
+    $(this).removeClass("normal");
+    $(this).addClass("destructMe");
+  }
+
+}
