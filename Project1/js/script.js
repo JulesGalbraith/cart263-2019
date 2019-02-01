b9215bc7fc075d2e0f2833950094c174a9224fa6
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2019-01-31 21:40:58 -0500

    Project1: altered div element movement setting from manual to jquery draggable, added text, buttons and text styles (with css), created new classes for different stages of fig rot

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -7,32 +7,23 @@ let whichFig;
 
 $(document).ready(function() {
 
-  $(".fig").on("dblclick", figPlucked);
+  $(".figSelected").hide();
+  $(".choice").hide();
+
+  $(".fig").draggable();
+  $(".fig").on("mousedown",figPlucked);
+
+  $("#taste").on("click",)
 
 
-
-
-  // this function was adapted (with help from Sabine) from an example at https://api.jquery.com/mousemove/ .
-  //it tracks the coordinates of the mouse in order to be able to bind a div element (a fig, in this case) to the cursor upon being clicked
-  $(".container").mousemove(function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-
-    if (figChosen === true) {
-      console.log("true");
-
-       $(whichFig).css("left", mouseX-40);
-       $(whichFig).css("top", mouseY-5);
-       $(".container").attr("cursor",url("../assets/images/holdingHand.png"));
-
-    }
-
-  });
-
-  function figPlucked() {
+function figPlucked(){
   figChosen = true;
-  whichFig = this;
-
+  $(".figSelected").show();
+  $(".choice").show();
+  console.log("hihi");
 }
 
-});
+function figRot(){
+  
+}
+  });
