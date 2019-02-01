29e19699689b67241c9b2dd304ed1099698217f1
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2019-02-01 11:46:29 -0500

    Project 3: set div elements (figs) to shake individually on mouseover

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -1,7 +1,9 @@
-let figs = [];
+let figs = [$(".fig")];
+let totalFigs =10;
 let mouseX;
 let mouseY;
 
+let shakeInterval= 3000
 let figChosen = false;
 let whichFig;
 
@@ -14,16 +16,27 @@ $(document).ready(function() {
   $(".fig").on("mousedown",figPlucked);
 
   $("#taste").on("click",)
+  $(".fig").on("mouseover",figShake);
 
+  $("#taste").on("click",rotFig)
+  
+});
 
 function figPlucked(){
   figChosen = true;
+  whichFig = $(this);
+  console.log(this);
   $(".figSelected").show();
   $(".choice").show();
-  console.log("hihi");
+
 }
 
-function figRot(){
-  
+function figShake(){
+      setInterval(shakeInterval,$(this).effect("shake",10,3));
+      console.log("shake");
+  }
+
+function rotFig(){
+  whichFig.attr("src","../assets/images/fig2.png");
+  console.log("ew");
 }
-  });
